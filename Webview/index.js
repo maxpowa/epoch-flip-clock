ec251b81a3a1501afa69ceb9f9e312df2bc0ee2b
Max Gurela <maxpowa1@gmail.com>
2016-07-18 15:25:36 -0700

    Add millis to clock

--- Webview/index.js
+++ Webview/index.js
@@ -1,87 +1,108 @@
-'use strict';
+"use strict";
 
 var digits = [];
 
 function getEpoch() {
-  return Date.now().toString().slice(0,-3);
+  return Date.now().toString();
 }
 
 function createDigit() {
-  var digit = document.createElement('digit');
-  digit.innerHTML = '\
+  var digit = document.createElement("digit");
+  digit.innerHTML =
+    "\
   <flap-top>          <n></n>   </flap-top>\
   <flap-top-flip>     <n></n>   </flap-top-flip>\
   <flap-bottom>       <n></n>   </flap-bottom>\
-  <flap-bottom-flip>  <n></n>   </flap-bottom-flip>';
+  <flap-bottom-flip>  <n></n>   </flap-bottom-flip>";
   return digit;
 }
 
 function flipDigitTo(digit, currentVal, updatedVal) {
-  var topFlapNum        = digit.querySelector('flap-top > n'),
-      topFlapFlip       = digit.querySelector('flap-top-flip'),
-      topFlapFlipNum    = topFlapFlip.querySelector('n'),
-      bottomFlapNum     = digit.querySelector('flap-bottom > n'),
-      bottomFlapFlip    = digit.querySelector('flap-bottom-flip'),
-      bottomFlapFlipNum = bottomFlapFlip.querySelector('n');
+  var topFlapNum = digit.querySelector("flap-top > n"),
+    topFlapFlip = digit.querySelector("flap-top-flip"),
+    topFlapFlipNum = topFlapFlip.querySelector("n"),
+    bottomFlapNum = digit.querySelector("flap-bottom > n"),
+    bottomFlapFlip = digit.querySelector("flap-bottom-flip"),
+    bottomFlapFlipNum = bottomFlapFlip.querySelector("n");
 
   topFlapNum.innerHTML = updatedVal;
   bottomFlapNum.innerHTML = currentVal;
 
   topFlapFlipNum.innerHTML = currentVal;
-  topFlapFlip.style.display = 'block';
+  topFlapFlip.style.display = "block";
 
   setTimeout(function() {
-    topFlapFlip.style.display = 'none';
+    topFlapFlip.style.display = "none";
   }, 300);
 
   setTimeout(function() {
     bottomFlapFlipNum.innerHTML = updatedVal;
-    bottomFlapFlip.style.display = 'block';
+    bottomFlapFlip.style.display = "block";
   }, 300);
 
   setTimeout(function() {
     bottomFlapNum.innerHTML = updatedVal;
-    bottomFlapFlip.style.display = 'none';
+    bottomFlapFlip.style.display = "none";
   }, 450);
 
-  digit.setAttribute('current-val', updatedVal);
+  digit.setAttribute("current-val", updatedVal);
+}
+
+function flipDigitNow(digit, currentVal, updatedVal) {
+  var topFlapNum = digit.querySelector("flap-top > n"),
+    bottomFlapNum = digit.querySelector("flap-bottom > n");
+
+  topFlapNum.innerHTML = updatedVal;
+  bottomFlapNum.innerHTML = updatedVal;
+
+  digit.setAttribute("current-val", updatedVal);
 }
 
 function updateClock() {
   var epoch = getEpoch(),
-      staggerDelay,
-      currentVal,
-      updatedVal,
-      i;
+    staggerDelay,
+    currentVal,
+    updatedVal,
+    i;
 
-  for (i = 0; i < epoch.length; i+=1) {
-    if(i === epoch.length-1) {
+  for (i = 0; i < epoch.length; i += 1) {
+    if (i === epoch.length - 1) {
       staggerDelay = 0;
     } else {
-      staggerDelay = Math.random() * 400;
+      staggerDelay = 0;
     }
-    currentVal = digits[i].getAttribute('current-val');
+    currentVal = digits[i].getAttribute("current-val");
     updatedVal = epoch[i];
-    if(currentVal !== updatedVal) {
-      setTimeout(flipDigitTo, staggerDelay, digits[i], currentVal, updatedVal);
+    if (currentVal !== updatedVal) {
+      if (i >= epoch.length - 3) {
+        flipDigitNow(digits[i], currentVal, updatedVal);
+      } else {
+        setTimeout(
+          flipDigitTo,
+          staggerDelay,
+          digits[i],
+          currentVal,
+          updatedVal
+        );
+      }
     }
   }
 }
 
 function setupClock() {
   var epoch = getEpoch(),
-      staggerDelay,
-      digit,
-      i;
+    staggerDelay,
+    digit,
+    i;
 
-  for (i = 0; i < epoch.length; i+=1) {
+  for (i = 0; i < epoch.length; i += 1) {
     digit = createDigit();
     staggerDelay = Math.random() * 400;
-    document.body.appendChild(digit);
+    document.querySelector("#ticker").appendChild(digit);
     setTimeout(flipDigitTo, staggerDelay, digit, null, epoch[i]);
   }
-  digits = document.querySelectorAll('digit');
+  digits = document.querySelectorAll("digit");
 }
 
 setupClock();
-setInterval(updateClock, 1000);
+setInterval(updateClock, 11);
